c95a59020a806ec7345c0793f612762290f5bb55
L <lnpeng@gmail.com>
2019-01-21 19:29:39 -0500

    Modify Resource class to an inner class.

--- src/ResourceFactory.java
+++ src/ResourceFactory.java
@@ -1,7 +1,3 @@
-class Resource {
-	public Resource() {}
-}
-
 public class ResourceFactory {
     private static class ResourceHolder {
         static final Resource instance = new Resource();
@@ -9,5 +5,8 @@ public class ResourceFactory {
 
     public static Resource getResource() {
         return ResourceHolder.instance;
+    }
+    
+    static class Resource {
     }
 }
